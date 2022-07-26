6a87a6ed3af94544fec5a4a8d1f19ff80f65a9b5
limyel <limyel@outlook.com>
2022-07-26 21:48:06 +0800

    init

---
+++ src/main/java/com/limyel/haoyuan/HaoyuanSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.limyel.haoyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaoyuanSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaoyuanSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/limyel/haoyuan/haoyuan/HaoyuanSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limyel.haoyuan.haoyuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaoyuanSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
