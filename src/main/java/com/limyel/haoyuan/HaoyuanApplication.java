6683532bf23bfc1cd2fb9f323457c238efe9e270
limyel <limyel@outlook.com>
2022-08-10 23:02:54 +0800

    rename application

--- src/main/java/com/limyel/haoyuan/HaoyuanApplication.java
+++ src/main/java/com/limyel/haoyuan/HaoyuanApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HaoyuanSpringBootApplication {
+public class HaoyuanApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HaoyuanSpringBootApplication.class, args);
+        SpringApplication.run(HaoyuanApplication.class, args);
     }
 
 }
